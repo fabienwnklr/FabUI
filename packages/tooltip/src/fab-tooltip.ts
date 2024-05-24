53a7f2e8b2927fed559b9d8d28e69d9defc4115e
fabienwnklr <fabien.winkler@outlook.fr>
2024-05-24 13:29:25 +0200

    feat(tooltip): add trigger event for show

--- packages/tooltip/src/fab-tooltip.ts
+++ packages/tooltip/src/fab-tooltip.ts
@@ -2,19 +2,33 @@ import { LitElement, html, unsafeCSS } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import styles from "./fab-tooltip.scss?inline";
 
+enum Trigger {
+  Hover = "hover",
+  Focus = "focus",
+}
+
+enum Position {
+  Bottom = "bottom",
+  Top = "top",
+  Left = "left",
+  Right = "right",
+}
 @customElement("fab-tooltip")
 export class FabTooltip extends LitElement {
   @property({ type: String })
   target = "";
 
-  @property({ type: String })
+  @property({ type: Position })
   position = "bottom";
 
   @property({ type: Boolean })
-  inline = true;
+  inline = false;
+
+  @property({ type: Trigger })
+  trigger = "focus";
 
   @property({ type: Boolean })
-  arrow = false;
+  arrow = true;
 
   private _uuid = Date.now().toString();
   private $tooltip: HTMLElement | null | undefined;
@@ -35,14 +49,31 @@ export class FabTooltip extends LitElement {
   }
 
   firstUpdated(): void {
+    this.$tooltip = this.shadowRoot?.getElementById(this._uuid);
     if (this.inline) {
       this._show();
+    } else {
+      if (!this.target) {
+        throw new Error("Missing property 'target' for triggering tooltip.");
+      }
+
+      const target = document.querySelector(this.target);
+
+      if (!target) {
+        throw new Error("target not find : " + this.target)
+      }
+      if (this.trigger === "hover") {
+        target.addEventListener("mouseover", this._show.bind(this));
+        target.addEventListener("mouseleave", this._hide.bind(this));
+      } else if (this.trigger === "focus") {
+        target.setAttribute("tabindex", "0");
+        target.addEventListener("focusin", this._show.bind(this));
+        target.addEventListener("focusout", this._hide.bind(this));
+      }
     }
   }
 
   _show() {
-    this.$tooltip = this.shadowRoot?.getElementById(this._uuid);
-
     this.$tooltip?.classList.add("show");
   }
 
